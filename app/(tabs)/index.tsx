1415166d76a03e5bca4e9d1a24d1d113db269bf1
Zephied <galmiche1105@gmail.com>
2026-04-14 12:04:39 +0200

    start index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Redirect } from 'expo-router';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function HomeScreen() {
   const [onboardingDone, setOnboardingDone] = useState<boolean | null>(null);
@@ -23,8 +23,11 @@ export default function HomeScreen() {
 
   return (
     <ThemedView style={styles.center}>
-      <FontAwesome name="book" size={64} color="#A1CEDC" />
-      <ThemedText type="title" style={styles.mainTitle}>Index</ThemedText>
+      <View style={styles.circle}>
+        <FontAwesome name="book" size={32} color="#fff" />
+      </View>
+      <ThemedText type="title" style={styles.mainTitle}>Bienvenue sur Mobile Library</ThemedText>
+      <ThemedText type="default">Explorez des milliers de livres grâce à l'API Open Library.</ThemedText>
     </ThemedView>
   );
 }
@@ -34,6 +37,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  circle: {
+    width: 48,
+    height: 48,
+    borderRadius: 40,
+    backgroundColor: '#A1CEDC',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
   },
   mainTitle: {
     marginBottom: 20,
