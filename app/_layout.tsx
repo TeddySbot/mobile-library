1710b440990006f63a6b98c2cf9903f7ac7b49be
lteddy <teddy.lemoal@ynov.com>
2026-04-27 10:14:57 +0200

    push

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,22 @@
+import { useColorScheme } from '@/hooks/use-color-scheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    'Axiforma-Bold': require('../assets/fonts/Axiforma-Bold.ttf'), // Vérifie bien le chemin
+  });
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -3,7 +3,11 @@ import { ThemedView } from '@/components/themed-view';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useRouter } from 'expo-router';
 import React from 'react';
-import { Button, StyleSheet } from 'react-native';
+import { Image, StyleSheet, TouchableOpacity } from 'react-native';
+import image3 from '../assets/onboarding/0010703188-L.jpg';
+import image2 from '../assets/onboarding/0012818414-L.jpg';
+import image1 from '../assets/onboarding/963821-L.jpg';
+
 
 export default function OnboardingScreen() {
   const router = useRouter();
@@ -14,35 +18,89 @@ export default function OnboardingScreen() {
   };
 
   return (
-    <ThemedView style={styles.bottom}>
-      <ThemedText type="title">Bienvenue sur Mobile Library</ThemedText>
-      <ThemedText type="default">Explorez des milliers de livres grâce à l'API Open Library.</ThemedText>
-    <Button 
-        title="Commencer" 
-        onPress={handleFinish} 
-        color="#A1CEDC",
-        style={{border-radius: 8;}}
-      />
-    
+    <ThemedView style={styles.mainContainer}>
+      <ThemedView style={styles.imageContainer}>
+        <ThemedView style={styles.skewCard}>
+          <Image source={image1} style={styles.skewImage} resizeMode="cover" />
+        </ThemedView>
+        <ThemedView style={styles.skewCard}>
+          <Image source={image2} style={styles.skewImage} resizeMode="cover" />
+        </ThemedView>
+        <ThemedView style={styles.skewCard}>
+          <Image source={image3} style={styles.skewImage} resizeMode="cover" />
+        </ThemedView>
+      </ThemedView>
+
+      <ThemedView style={styles.textContainer}>
+        <ThemedText type="title" style={styles.textCenter}>Bienvenue sur Mobile Library</ThemedText>
+        <ThemedText type="default" style={styles.textCenter}>
+          Explorez des milliers de livres grâce à l'API Open Library.
+        </ThemedText>
+      </ThemedView>
+
+      <ThemedView style={styles.bottomContainer}>
+        <TouchableOpacity style={styles.customButton} onPress={handleFinish}>
+          <ThemedText style={styles.buttonText}>Commencer</ThemedText>
+        </TouchableOpacity>
+      </ThemedView>
     </ThemedView>
-    
   );
 }
 
 const styles = StyleSheet.create({
-  center: {
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#121212',
+  },
+  imageContainer: {
+    marginTop: 60, 
+    alignItems: 'center',
+    gap: 10,
+    zIndex: 1, 
+  },
+  skewCard: {
+    width: '140%',
+    height: 120,
+    overflow: 'hidden',
+    transform: [{ rotate: '-12deg' }],
+  },
+  skewImage: {
+    width: '100%',
+    height: 180,
+    transform: [{ rotate: '12deg' }, { scale: 1.4 }],
+    top: -30,
+  },
+  textContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    gap: 15,
+    paddingHorizontal: 30,
+    backgroundColor: 'transparent',
+    zIndex: 2,
   },
-  bottom: {
-    marginBottom: 60,
-    flex: 1,
-    justifyContent: 'flex-end',
+  textCenter: {
+    textAlign: 'center',
+    fontFamily: 'Axiforma-Bold',
+    color: '#FFFFFF',
+  },
+  bottomContainer: {
+    paddingBottom: 60,
     alignItems: 'center',
-    gap: 12,
   },
-  mainTitle: {
-    marginBottom: 20,
-  }
+  customButton: {
+    width: '80%',
+    maxWidth: 300,
+    height: 55,
+    borderRadius: 30,
+    borderWidth: 1.5,
+    borderColor: '#00FFFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#00FFFF',
+  },
 });
